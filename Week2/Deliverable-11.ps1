cfa53240dd40424cc645345382dff86a2d9c2806
Emma <emma.sullivan@mymail.champlain.edu>
2025-09-08 13:02:21 -0400

    in progress

---
+++ Week2/Deliverable-11.ps1
@@ -0,0 +1,7 @@
+﻿# Without changing the directory (do your operations recursively without getting in to the child directory)
+# Find ever .csv file recursively and change their extensions to .log
+# Recursively display all the files (not directories)
+
+$files = - - 
+$files | Where-Object - {$_.Extension - '.csv', '.log'}
+Get-
