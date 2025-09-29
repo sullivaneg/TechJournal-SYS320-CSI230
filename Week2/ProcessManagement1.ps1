c55f73a1f320a7127f617bbcc0777f2abffacc6d
Emma <emma.sullivan@mymail.champlain.edu>
2025-09-29 17:35:48 -0400

    Process Management 1 Lab

---
+++ Week2/ProcessManagement1.ps1
@@ -0,0 +1,8 @@
+﻿# Write a PowerShell script that lists every procss for which ProcessName starts with 'C'.
+Get-Process -Name C*
+
+# Write a Powershell script that lists every process for which the path does not include the string 'system32'
+(Get-Process | Where-Object {$_.Path -inotlike "*system32*"} | Select-Object Id, ProcessName, Path)
+
+# Write a Powershell script that lists every stopped service, orders it alphabetically and saves it to a csv file
+(Get-Service | Where-Object {$_.Status -eq "Stopped"} | Sort-Object | Export-Csv -Path .\StoppedServices.csv)
---
+++ Week2/ProcessManagement_Script4.ps1
@@ -0,0 +1,20 @@
+﻿#Write a PowerShell Script that 
+# a) if an instance of it is not running already, it starts Google Chrome Web Browser and Directs it Champlain.edu
+# b) if an instance is already running, stops it
+
+# Creat a variable for Chrome and Champlain URL
+$chrome_tab = (Get-Process |Where-Object {$_.Name -eq "chrome"})
+$champlain = "https://www.champlain.edu"
+
+# Check if it exists
+if ($chrome_tab -eq $null){
+    echo "There is no current chrome processes running, starting a new one..."
+    Start-Process -FilePath "chrome.exe" -ArgumentList $champlain
+    }
+else {
+    echo "Found a chrome process running, stopping process..."
+    Stop-Process -Name "chrome"
+    }
+
+# Verify
+Get-Process -Name "chrome"
