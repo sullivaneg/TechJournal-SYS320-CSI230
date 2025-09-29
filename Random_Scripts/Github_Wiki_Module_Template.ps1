307effb399c54b05ccaa4632e9f807589f6892c7
Emma <emma.sullivan@mymail.champlain.edu>
2025-09-29 18:24:11 -0400

    For generating github modules for my home wiki

---
+++ Random_Scripts/Github_Wiki_Module_Template.ps1
@@ -0,0 +1,19 @@
+﻿# Make Markdown for each new Module 
+$mod_num = Read-Host -Prompt "Module #"
+$mod_name = Read-Host -Prompt "Module Name"
+$num_labs = [int](Read-Host "How many labs in this module?")
+
+$startline = "## Module ${mod_num}: $mod_name"
+
+$body = (1..$num_labs | ForEach-Object {
+    $lab_name = Read-Host -Prompt "Lab Name"
+    @"
+> [[${lab_name}]]
+> 
+"@   
+})
+
+echo @" 
+$startline
+$($body -join "`n")
+"@
