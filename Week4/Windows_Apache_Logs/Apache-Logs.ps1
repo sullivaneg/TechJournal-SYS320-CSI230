4991ffc428e9be6ef25b69c1f843a1bde64c86f3
Emma <emma.sullivan@mymail.champlain.edu>
2025-10-21 21:42:39 -0400

    Last deliverable

--- Week4/Windows_Apache_Logs/Apache-Logs.ps1
+++ Week4/Windows_Apache_Logs/Apache-Logs.ps1
@@ -1,4 +1,4 @@
-﻿# Write a dunction called Apache-Logs.ps1 that will take 3 inputs:
+﻿# Write a function called Apache-Logs.ps1 that will take 3 inputs:
 # A) The page visited or referred from (index.html, page1.html,...)
 # B) HTTP Code Returned
 # C) Name of the Web Browser
@@ -14,29 +14,23 @@ Output: IP addresses that have visited or referred from, with the given web brow
         and got the given HTTP response.
 ***********************************************************************************************#>
 
-function getVistorIps($page, $http, $browser){
+function getVisitorIps($page, $http, $browser){
 
-# Get only logs that contain the status code, page, and browser name
-$bypage = Get-Content C:\xampp\apache\logs\access.log | Select-String " $page "
-$bycode = Get-Content $bypage | Select-String " $http "
-$bybrowser = Get-Content $bycode | Select-String " $browser "
+    # Define a Regex for IP addresses
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 
-# Define a Regex for IP addresses
-$regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+    # Get only logs that contain the status code, page, and browser name
+    $logs = Get-Content C:\xampp\apache\logs\access.log | `
+        Select-String $page | Select-String $http | Select-String $browser
 
-# Get strings in $bybrowser that match the regex
-$ipsUnorganized = $regex.matches($bybrowser)
+   #Get $ips record that match the regex
+   $ipsUnorganized = $regex.Matches($logs)
 
-# ips as a customobject
-$ips = @()
-for ($i=0; $i -lt $ipsUnorganized.Count; $i++){
-    $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].value;}
+   #Initiate Array
+   $ips = @()
+   foreach ($ip in $ipsUnorganized) {
+       #Create the Custom Object
+       $ips += [pscustomobject]@{ "IP" = $ip.value }
+   }
+   Return $ips
 }
-
-$ips | Where-Object { $_.IP -ilike "10.*" }
-}
-
-# Testing Section
-# Status: 404, Browser: Mozilla, index.html
-
-getVistorIps(" index.html ", " 404 ", "Mozilla")
---
+++ Week4/Windows_Apache_Logs/main.ps1
@@ -0,0 +1,27 @@
+﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+
+<#******************************************************************************
+Function: Counts how many times each Ip shows up in the output array from getVisitorIps
+Input: Array
+Output: Number of times visited and Ip Address
+Note: I'm going to comment out the IPs needing to be 10.* because my Kali IP starts with 140
+******************************************************************************#>
+function ipCounts($ips){
+    $ipsoftens = $ips #| Where-Object { $_.IP -ilike "10.*" }
+    $counts = $ipsoftens | group IP
+    $counts | Select-Object Count, Name
+    Write-Host $counts
+    }
+
+
+
+# Testing Section
+
+# Status: 304, Browser: Mozilla, page1.html
+# Mozilla is my Kali Browser
+$ips = getVisitorIps "/page1.html" " 304 " "Mozilla"
+ipCounts $ips
+
+#Status: 200, Browser: Chrome, Page: Any
+$ips = getVisitorIps "/*" " 200 " "Chrome"
+ipCounts $ips
