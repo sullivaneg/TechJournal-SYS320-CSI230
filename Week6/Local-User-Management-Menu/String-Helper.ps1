28093f60bd6419448d8984ce5d0cfff3c4a0e384
Emma <emma.sullivan@mymail.champlain.edu>
2025-10-27 22:51:51 -0400

    Stopping Point

---
+++ Week6/Local-User-Management-Menu/String-Helper.ps1
@@ -0,0 +1,49 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+
+<#*************************************************************
+Functions: checks if the password is at least 6 characters, if is contains 1 special character, 1 number,
+           and 1 letter
+Input: 1) Password
+Output: 1) True if the string meets requirements
+        2) False if it doesn't
+*************************************************************#>
+function checkPassword($password) {
+    $regex = [regex] "^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9])(.{7,})$"
+    $reqs = $password -match $regex
+
+    if($reqs) {
+        return True
+        }
+    else {
+        return False
+        }
+}
---
+++ Week6/Local-User-Management-Menu/main-1.ps1
@@ -0,0 +1,170 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - List Enabled Users`n"
+$Prompt += "2 - List Disabled Users`n"
+$Prompt += "3 - Create a User`n"
+$Prompt += "4 - Remove a User`n"
+$Prompt += "5 - Enable a User`n"
+$Prompt += "6 - Disable a User`n"
+$Prompt += "7 - Get Log-In Logs`n"
+$Prompt += "8 - Get Failed Log-In Logs`n"
+$Prompt += "9 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 9){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $enabledUsers = getEnabledUsers
+        Write-Host ($enabledUsers | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){
+        $notEnabledUsers = getNotEnabledUsers
+        Write-Host ($notEnabledUsers | Format-Table | Out-String)
+    }
+
+
+    # Create a user
+    elseif($choice -eq 3){ 
+
+        $name = Read-Host -Prompt "Please enter the username for the new user"
+        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+
+        $usrflag = checkUser($name)
+        if($usrflag) {
+            Write-Host "This user already exists!"
+            exit
+            }
+        else {
+            $passflag = checkPassword($password)
+            if($passflag) {
+                createAUser $name $password
+
+                Write-Host "User: $name is created." | Out-String
+
+                }
+            else {
+                Write-Host "Password doesn't meet requirements"
+                exit
+                }
+    }
+
+
+    # Remove a user
+    elseif($choice -eq 4){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be removed"
+
+        $exists = checkUser $name
+
+        if($exists){
+            removeAUser $name
+            Write-Host "User: $name Removed." | Out-String
+        }
+        else {
+            Write-Host "User does not exist"
+            exit
+    }
+
+
+    # Enable a user
+    elseif($choice -eq 5){
+
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
+
+        $exists = checkUser $name
+
+        if($exists) {
+
+            enableAUser $name
+
+            Write-Host "User: $name Enabled." | Out-String
+        }
+        else {
+            Write-Host "User Does Not exist"
+            exit
+        }
+    }
+
+
+    # Disable a user
+    elseif($choice -eq 6){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
+
+        $exists = checkUser $name
+
+        if($exists) {
+
+            disableAUser $name
+
+            Write-Host "User: $name Disabled." | Out-String
+        }
+
+        else {
+            write-Host "User Does Not Exist"
+            exit
+        }
+    }
+
+
+    elseif($choice -eq 7){
+
+        $name = Read-Host -Prompt "Please enter the username for the user logs"
+
+        # TODO: Check the given username with the checkUser function.
+
+        $userLogins = getLogInAndOffs 90
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 8){
+
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+
+        # TODO: Check the given username with the checkUser function.
+
+        $userLogins = getFailedLogins 90
+        # TODO: Change the above line in a way that, the days 90 should be taken from the user
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    # TODO: Create another choice "List at Risk Users" that
+    #              - Lists all the users with more than 10 failed logins in the last <User Given> days.  
+    #                (You might need to create some failed logins to test)
+    #              - Do not forget to update prompt and option numbers
+    
+    # TODO: If user enters anything other than listed choices, e.g. a number that is not in the menu   
+    #       or a character that should not be accepted. Give a proper message to the user and prompt again.
+    
+
+}
+
+
+
+
