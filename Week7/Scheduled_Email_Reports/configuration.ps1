1543f498e74d04d022c3d6b4d0401ddc4adfb49e
Emma <emma.sullivan@mymail.champlain.edu>
2025-11-17 12:18:56 -0500

    saving before I try something

--- Week7/Scheduled_Email_Reports/configuration.ps1
+++ Week7/Scheduled_Email_Reports/configuration.ps1
@@ -1,49 +1,64 @@
-﻿$Prompt = "`n"
-$Prompt += "Please choose your operation:`n"
-$Prompt += "1 - Show Configuration`n"
-$Prompt += "2 - Change Configuration`n"
-$Prompt += "3 - Exit`n"
+﻿function readConfiguration {
+    $config_table = @()
+    $file_content = Get-Content -Path $file_path
 
-$operation = $True
-$file_path = "~\TechJournal-SYS320-CSI230\Week7\Scheduled_Email_Reports\configuration.txt"
-
-while($operation){
-
-    
-    Write-Host $Prompt | Out-String
-    $choice = Read-Host 
-
-
-    if($choice -eq 3){
-        Write-Host "`nGoodbye" | Out-String
-        exit
-        $operation = $false 
-    }
-
-    elseif($choice -eq 1){
-        $config_table = @()
-        $file_content = Get-Content -Path $file_path
-
-        $config_table +=[pscustomobject]@{"Days Evaluating" = $file_content[0];`
-                                          "Time Scheduled" = $file_content[1];`
+    $config_table +=[pscustomobject]@{"Days Evaluating" = $file_content[0];`
+                                      "Time Scheduled" = $file_content[1];`
                                           }
-        Write-Host "`nCurrent Configuration"
-        $config_table | Format-Table -Autosize
+    Write-Host "`nCurrent Configuration"
+    $config_table | Format-Table -Autosize
+}
 
-    }
-
-    elseif($choice -eq 2){
-        Write-Host "`n"
+function changeConfiguration {
+Write-Host "`n"
         Write-Host "Changing your configuration...`n"
+
         $days = Read-Host -Prompt "How many days of logs would you like?"
         $time = Read-Host -Prompt "What execution time would you like (HH:MM AM/PM)"
 
         Set-Content -Path $file_path -Value $days, $time
         Write-Host "`nFile Changed"
-    }
-
-    else {
-        Write-Host "Please choose a valid option"
-        continue
-        }
 }
+
+function configurationMenu($file_path){
+
+    $Prompt = "`n"
+    $Prompt += "Please choose your operation:`n"
+    $Prompt += "1 - Show Configuration`n"
+    $Prompt += "2 - Change Configuration`n"
+    $Prompt += "3 - Exit`n"
+
+    $operation = $True
+
+    while($operation){
+
+    
+        Write-Host $Prompt | Out-String
+        $choice = Read-Host 
+
+
+        if($choice -eq 3){
+            Write-Host "`nGoodbye" | Out-String
+            exit
+            $operation = $false 
+        }
+
+        elseif($choice -eq 1){
+            readConfiguration
+
+        }
+
+        elseif($choice -eq 2){
+            changeConfiguration
+        }
+
+        else {
+            Write-Host "Please choose a valid option"
+            continue
+            }
+    }
+}
+
+# Main
+$file_path = "~\TechJournal-SYS320-CSI230\Week7\Scheduled_Email_Reports\configuration.txt"
+configurationMenu $file_path
