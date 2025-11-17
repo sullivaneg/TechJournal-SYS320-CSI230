8b8a1102e132e6f8ae50c2ec568df5839a0f4f3a
Emma <emma.sullivan@mymail.champlain.edu>
2025-11-17 15:38:24 -0500

    Saving before I mess it up

--- Week7/Scheduled_Email_Reports/configuration.ps1
+++ Week7/Scheduled_Email_Reports/configuration.ps1
@@ -1,15 +1,17 @@
-﻿function readConfiguration {
+﻿function readConfiguration($file_path) {
     $config_table = @()
     $file_content = Get-Content -Path $file_path
 
-    $config_table +=[pscustomobject]@{"Days Evaluating" = $file_content[0];`
-                                      "Time Scheduled" = $file_content[1];`
+    $days = $file_content[0]
+    $time = $file_content[1]
+
+    $config_table +=[pscustomobject]@{"Days Evaluating" = $days;`
+                                      "Time Scheduled" = $time;`
                                           }
-    Write-Host "`nCurrent Configuration"
     $config_table | Format-Table -Autosize
 }
 
-function changeConfiguration {
+function changeConfiguration($file_path) {
 Write-Host "`n"
         Write-Host "Changing your configuration...`n"
 
@@ -27,15 +29,12 @@ Write-Host "`n"
         } while (-not $inputValid)
 
         #Time Input Validation
-        $time = 0
-        $temp = 0
         $inputValid = $false
 
         do {
-            $try_time = Read-Host -Prompt "What execution time would you like (H:MM AM/PM)"
-            $time = $try_time
+            $time = Read-Host -Prompt "What execution time would you like (H:MM AM/PM)"
 
-            if ([DateTime]::TryParseExact($try_time, "h:mm tt", $null, [System.Globalization.DateTimeStyles]::None, [ref]$temp)) {
+            if ($time -match "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]\s[AP]M") {
                 $inputValid = $true
             } else {
                 Write-Host "`nInvalid Input: Please enter a time in the format (H:MM AM/PM)`n" -ForegroundColor Red
@@ -70,12 +69,12 @@ function configurationMenu($file_path){
         }
 
         elseif($choice -eq 1){
-            readConfiguration
+            readConfiguration $file_path
 
         }
 
         elseif($choice -eq 2){
-            changeConfiguration
+            changeConfiguration $file_path
         }
 
         else {
@@ -86,5 +85,5 @@ function configurationMenu($file_path){
 }
 
 # Main
-$file_path = "~\TechJournal-SYS320-CSI230\Week7\Scheduled_Email_Reports\configuration.txt"
-configurationMenu $file_path
+#$file_path = "~\TechJournal-SYS320-CSI230\Week7\Scheduled_Email_Reports\configuration.txt"
+#configurationMenu $file_path
