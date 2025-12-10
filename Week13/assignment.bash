07f16144636962c1d09b7d6477170c7c38a64289
Emma <143442079+sullivaneg@users.noreply.github.com>
2025-12-09 21:14:20 -0500

    m

--- Week13/assignment.bash
+++ Week13/assignment.bash
@@ -4,46 +4,30 @@
 link="10.0.17.47/Assignment.html"
 
 fullPage=$(curl -sL "$link")
-#toolOutput=$(echo "$fullPage" | \
-#xmlstarlet format --html --recover 2>/dev/null | \
-#xmlstarlet select --template --copy-of \
-#"//html//body//table//tr")
-
-#echo "$toolOutput" | sed 's/<\/tr>/\n/g' | \
-                    # sed -e 's/&amp;//g' | \
-                     #sed -e 's/<tr>//g' | \
-                     #sed -e 's/<td[^>]*>//g' | \
-                     #sed -e 's/<\/td>/;/g' | \
-                     #sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
-                     #sed -e 's/<[/\]\{0,1\}nobr>//g' \
-               # > tables.txt
-rows=$(echo "$fullPage" | \
-xmlstarlet fo --html --recover 2>/dev/null | \
-xmlstarlet sel -t -m "//table/tr" -c "." -n)
 
 # Temp
-temp=$(echo "$rows" |
-grep -A10 "Temperature" |
-sed 's/<[^>]*>//g' |
-grep -v Temperature | grep -v Date | sed '/^\s*$/d')
+temp=$(echo "$fullPage" |
+xmlstarlet format | \
+xmlstarlet select -t -m "//table[@id='temp']/tr" -v "td[1]" -n) 
 
 # Pressure
-pressure=$(echo "$rows" |
-grep -A10 "Pressure" |
-sed 's/<[^>]*>//g' |
-grep -v Pressure | grep -v Date | sed '/^\s*$/d')
+pressure=$(echo "$fullPage" |
+xmlstarlet format | \
+xmlstarlet select -t -m "//table[@id='press']/tr" -v "td[1]" -n)
 
-# Covert to Arrays
-tempValues=($(echo "$temp"))
-pressureValues=($(echo "$pressure"))
+# time
+dtime=$(echo "$fullPage" |
+xmlstarlet format | \
+xmlstarlet select -t -m "//table[@id='temp']/tr" -v "td[2]" -n)
 
-len=${#tempValues[@]}
+# Loop count
+count=$(echo "$temp" | wc -l)
+for ((i=1; i<=count; i++));
+do
+    temp=$(echo "$temp" | head -n $i | tail -n 1)
+    press=$(echo "$pressure" | head -n $i | tail -n 1)
+    dtime=$(echo "$dtime" | head -n $i | tail -n 1)
 
-# Merge and Print Tables
-for ((i=0; i<$len; i+=2)); do
-    t="${tempValues[$i]}"
-    date="${tempValues[$i+1]}"
-    p="${pressureValues[$i]}"
-    echo "$p $t $date"
+    echo -e "$press $temp $dtime"
 done
     
