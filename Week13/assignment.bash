d8a068d6287eaff6129be8dbd71e5e0a33d38880
Emma <143442079+sullivaneg@users.noreply.github.com>
2025-12-09 21:24:48 -0500

    finished reading tables lab

--- Week13/assignment.bash
+++ Week13/assignment.bash
@@ -1,33 +1,21 @@
-#! /bin/bash
+#!/bin/bash
 
-# Using xmlstarlet to extract table
 link="10.0.17.47/Assignment.html"
-
 fullPage=$(curl -sL "$link")
 
-# Temp
-temp=$(echo "$fullPage" |
-xmlstarlet format | \
-xmlstarlet select -t -m "//table[@id='temp']/tr" -v "td[1]" -n) 
+# Read into arrays
+mapfile -t temp_array < <(echo "$fullPage" | xmlstarlet format | \
+xmlstarlet select -t -m "//table[@id='temp']/tr" -v "td[1]" -n)
 
-# Pressure
-pressure=$(echo "$fullPage" |
-xmlstarlet format | \
+mapfile -t press_array < <(echo "$fullPage" | xmlstarlet format | \
 xmlstarlet select -t -m "//table[@id='press']/tr" -v "td[1]" -n)
 
-# time
-dtime=$(echo "$fullPage" |
-xmlstarlet format | \
+mapfile -t time_array < <(echo "$fullPage" | xmlstarlet format | \
 xmlstarlet select -t -m "//table[@id='temp']/tr" -v "td[2]" -n)
 
-# Loop count
-count=$(echo "$temp" | wc -l)
-for ((i=1; i<=count; i++));
+# Loop
+for ((i=0; i<${#temp_array[@]}; i++));
 do
-    temp=$(echo "$temp" | head -n $i | tail -n 1)
-    press=$(echo "$pressure" | head -n $i | tail -n 1)
-    dtime=$(echo "$dtime" | head -n $i | tail -n 1)
-
-    echo -e "$press $temp $dtime"
+    echo -e "${press_array[i]} ${temp_array[i]} ${time_array[i]}"
 done
-    
+
